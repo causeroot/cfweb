3bdecb0a05fe82525c8c6c60fe35238463640772
Gardner\!\! <gbickfor@adobe.com>
2014-02-18 01:06:01 -0800

    Sorting and handlebars working.

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,157 @@
-var attr = Ember.attr, hasMany = Ember.hasMany;
-
 var challenges = null;
 
-App = Ember.Application.create();
+var props = Array();
+props['deadline'] = false;
+props['post_date'] = false;
+props['award'] = false;
 
-App.Router.map(function() {
-  this.resource('challenge', function() {
-    this.resource('challenge', { path: ':id' });
-  });
-});
+function render_challenges() {
+  var source   = $("#index").html();
+  var template = Handlebars.compile(source);
+  var results = template({challenges: challenges});
+  $("#content").html(results)
+}
 
-App.IndexRoute = Ember.Route.extend({
-  //
-});
-
-App.ChallengeController = Ember.ObjectController.extend({
-  
-});
-
-App.ChallengesRoute = Ember.Route.extend({
-  model: function(params) {
-      console.log("model function.");
-      return Ember.$.getJSON("js/data.json").then(function(data) {
-        challenges = data;
-        return data;
-      });
-    }
-});
-
-App.ChallengeRoute = Ember.Route.extend({
-  model: function(params) {
-    if (null == challenges) {
-      return Ember.$.getJSON("js/data.json").then(function(data) {
-        challenges = data;
-        Ember.$.map(challenges, function(challenge) {
-            if(challenge.id == params.id) {
-              console.log("returning challenge");
-              return challenge;
-            }
-        });
-      });
-    } else {
-      Ember.$.map(challenges, function(challenge) {
-          if(challenge.id == params.id) {
-            console.log("returning challenge");
-            console.log(challenge);
-            return challenge;
-          }
-      });
-    }
+function sort_challenges(c, prop) {
+  challenges = c;
+  for (i = 0; i < c.length; i++) {
+    c[i].awards = sort_awards(c[i].awards);
+    c[i].deadlines = sort_deadlines(c[i].deadlines);
   }
-});
+  cf_sort(c, prop);
+  return c;
+}
 
-Handlebars.registerHelper('maxAward', function(context, options) {
-    var ret = 0;
-    console.log(context);
-    for (var i = 0; i < context.length; i++) {
-      if (context[i].value) {
-        val = Number(context[i].value.replace(/[^0-9\.]+/g,""));
-        if (ret < val) {
-          ret = val;
-        }
+function sort_awards(awards) {
+  return awards.sort(function(a, b) {
+    if (a.value && b.value) {
+      var aval = Number(a.value.replace(/[^0-9\.]+/g,""));
+      var bval = Number(b.value.replace(/[^0-9\.]+/g,""));
+      if (aval == bval) return 0;
+      if (a.value > b.value) {
+        return -1;
+      } else {
+        return 1;
       }
     }
-    return ret;
-});
+  });
+}
+
+function sort_deadlines(deadlines) {
+  return deadlines.sort(function(a, b) {
+    if (a.date && b.date) {
+      var date1 = new Date(a.date);
+      var date2 = new Date(b.date);
+      if (date1.getTime() == date2.getTime()) {
+        console.log(a.date + ' is same a ' + b.date);
+        return 0;
+      }
+      if (date1.getTime() > date2.getTime()) {
+        return -1;
+        console.log(a.date + ' is larger than ' + b.date);
+      } else {
+        return 1;
+      }
+    } else {
+      console.log(a.date + ' is error in relation to ' + b.date);
+    }
+  });
+}
+
+function cf_sort(c, prop) {
+  var asc = !props[prop];
+  props[prop] = asc;
+  
+  if ('deadline' == prop) {
+    return sort_by_deadline(c, asc)
+  } else if ('award' == prop) {
+    return sort_by_award(c, asc);
+  } else if ('post_date' == prop) {
+    return sort_by_date(c, asc);
+  }
+}
+
+function sort_by_deadline(c, asc) {
+  challenges = challenges.sort(function(a, b) {
+    if ((a.deadlines[0] && b.deadlines[0]) && (a.deadlines[0].date && b.deadlines[0].date)) {
+      var date1 = new Date(a.deadlines[0].date);
+      var date2 = new Date(b.deadlines[0].date);
+      if (date1.getTime() == date2.getTime()) {
+        if (a.id > b.id) {
+          return (asc) ? -1 : 1;
+        } else {
+          return (asc) ? 1 : -1;
+        }
+      }
+      if (date1.getTime() > date2.getTime()) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else {
+      if (a.id < b.id) {
+        return (asc) ? -1 : 1;
+      } else {
+        return (asc) ? 1 : -1;
+      }
+    }
+  });
+}
+
+function sort_by_award(c, asc) {
+  return c.sort(function(a, b) {
+    if (a.awards[0] && b.awards[0]) {
+      if (a.awards[0].value && b.awards[0].value) {
+        var aval = Number(a.awards[0].value.replace(/[^0-9\.]+/g,""));
+        var bval = Number(b.awards[0].value.replace(/[^0-9\.]+/g,""));
+        if (aval == bval) {
+          if (a.id > b.id) {
+            return (asc) ? -1 : 1;
+          } else {
+            return (asc) ? 1 : -1;
+          }
+        }
+        if (aval > bval) {
+          return (asc) ? -1 : 1;
+        } else {
+          return (asc) ? 1 : -1;
+        }
+      } else {
+        return 0;
+      }
+    }
+    });  
+}
+
+function sort_by_date(c, asc){
+  return c.sort(function(a, b) {
+    var date1 = new Date(a.post_date);
+    var date2 = new Date(b.post_date);
+    if (date1.getTime() == date2.getTime()) {
+      console.log("dates are the same");
+      if (a.id > b.id) {
+        return (asc) ? -1 : 1;
+      } else {
+        return (asc) ? 1 : -1;
+      }
+    }
+    if (date1.getTime() > date2.getTime()) {
+      console.log("dates are bigger");
+      return (asc) ? -1 : 1;
+    } else {
+      console.log("dates are smaller");
+      return (asc) ? 1 : -1;
+    }
+  });
+}
+
+function findChallenge(id) {
+  $.map(challenges, function(challenge) {
+      if(challenge.id == id) {
+        console.log("returning challenge");
+        console.log(challenge);
+        return challenge;
+      }
+  });
+}
+      
--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,31 +1,8 @@
-// in order to see the app running inside the QUnit runner
-App.rootElement = '#ember-testing';
-
-// Common test setup
-App.setupForTesting();
-App.injectTestHelpers();
-
-// common QUnit module declaration
-module("Integration tests", {
-  setup: function() {
-    // before each test, ensure the application is ready to run.
-    Ember.run(App, App.advanceReadiness);
-  },
-
-  teardown: function() {
-    // reset the application state between each test
-    App.reset();
-  }
-});
-
 // QUnit test case
-test("/", function() {
-  // async helper telling the application to go to the '/' route
-  visit("/");
-
+test("test sort", function() {
   // helper waiting the application is idle before running the callback
-  andThen(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
-    equal(find("li").length, 3, "There are three items in the list");
-  });
+  var challenge = findChallenge(7);
+  console.log(challenge);
+  ok(challenge['id'] == 7);
+  console.log("inside test");
 });
